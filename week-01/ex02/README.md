c860929355a534acd9eef3d1f784cb1937ff207d
khduyentr <khduyentr2212@gmail.com>
2023-07-27 16:10:15 +0700

    not refactor code

--- week-01/ex02/README.md
+++ week-01/ex02/README.md
@@ -1,35 +1,27 @@
 # Integer
-`go run sorter.go int 1 0 3 4 7`
+`go run sorter.go -int 1 0 3 4 7`
 
 Output: `0 1 3 4 7`
 
-`go run sorter.go int 1 0 3 4 7 -d`
-
-Output: `7 4 3 1 0`
-
 # Float
-`go run sorter.go float 1.1 0.5 3.2 4.4 7.5`
+`go run sorter.go -float 1.1 0.5 3.2 4.4 7.5`
 
 Output: `0.5 1.1 3.2 4.4 7.5`
 
-`go run sorter.go float 1.1 0.5 3.2 4.4 7.5 -d`
-
-Output: `7.5 4.4 3.2 1.1 0.5`
 
 # String
-`go run sorter.go string apple dog cat bird egg`
+`go run sorter.go -string apple dog cat bird egg`
 
 Output: `apple bird cat dog egg`
 
-`go run sorter.go string apple dog cat bird egg -d`
-
-Output: `egg dog cat bird apple`
 
 # Mix
 `go run sorter.go mix 1 apple 1.1 banana 0.5 cucumber 3.2 dog 4.4 7.5 5`
 
 Output: `0.5 1 1.1 3.2 4.4 5 7.5 apple banana cucumber`
 
-`go run sorter.go mix 1 apple 1.1 banana 0.5 cucumber 3.2 dog 4.4 7.5 5 -d`
+# Error: Invalid data type
 
-Output: `cucumber banana apple 7.5 5 4.4 3.2 1.1 1 0.5`
+`go run sorter.go -int 1 0 3 4 7 hehe`
+
+Output: `Invalid datatype - exit status 1`
--- week-01/ex02/algo/selection_sort.go
+++ week-01/ex02/algo/selection_sort.go
@@ -4,45 +4,27 @@ import (
 	"github.com/khduyentr/go-23/w01/ex02/helper"
 )
 
-func SelectionSort[Type helper.AvailableType](array []Type, isDescending bool) {
+func SelectionSort[Type helper.AvailableType](array []Type) {
 	arrayLen := len(array)
 
-	if !isDescending { // ascending
-		for i := 0; i < arrayLen-1; i++ {
-			minIndex := i
-			for j := i + 1; j < arrayLen; j++ {
-				if array[j] < array[minIndex] {
-					minIndex = j
-				}
+	for i := 0; i < arrayLen-1; i++ {
+		minIndex := i
+		for j := i + 1; j < arrayLen; j++ {
+			if array[j] < array[minIndex] {
+				minIndex = j
 			}
-
-			array[i], array[minIndex] = array[minIndex], array[i]
 		}
-	} else { // descending
-		for i := 0; i < arrayLen-1; i++ {
-			maxIndex := i
-			for j := i + 1; j < arrayLen; j++ {
-				if array[j] > array[maxIndex] {
-					maxIndex = j
-				}
-			}
 
-			array[i], array[maxIndex] = array[maxIndex], array[i]
-		}
+		array[i], array[minIndex] = array[minIndex], array[i]
 	}
 
 	helper.PrintArray(array)
 }
 
-func MixSelectionSort(array []interface{}, isDescending bool) {
+func MixSelectionSort(array []interface{}) {
 
 	floatArray, stringArray := helper.ClassifyArrayBasedOnType(array)
 
-	if !isDescending {
-		SelectionSort(floatArray, isDescending)
-		SelectionSort(stringArray, isDescending)
-	} else {
-		SelectionSort(stringArray, isDescending)
-		SelectionSort(floatArray, isDescending)
-	}
+	SelectionSort(floatArray)
+	SelectionSort(stringArray)
 }
