399f5a1fe33ed5aee7eb38388efb03cb7c61e54f
khduyentr <khduyentr2212@gmail.com>
2023-08-03 10:40:41 +0700

    unit test ex02

--- week-01/ex02/algo/insertion_sort.go
+++ week-01/ex02/algo/insertion_sort.go
@@ -4,45 +4,28 @@ import (
 	"github.com/khduyentr/go-23/w01/ex02/helper"
 )
 
-func InsertionSort[Type helper.AvailableType](array []Type, isDescending bool) {
+func InsertionSort[Type helper.AvailableType](array []Type) []Type {
 	arrayLen := len(array)
 
-	if !isDescending {
-		for i := 1; i < arrayLen; i++ {
-			last := array[i]
-			j := i
+	for i := 1; i < arrayLen; i++ {
+		last := array[i]
+		j := i
 
-			for j > 0 && array[j-1] > last {
-				array[j] = array[j-1]
-				j = j - 1
-			}
-
-			array[j] = last
+		for j > 0 && array[j-1] > last {
+			array[j] = array[j-1]
+			j = j - 1
 		}
-	} else {
-		for i := 1; i < arrayLen; i++ {
-			last := array[i]
-			j := i
 
-			for j > 0 && array[j-1] < last {
-				array[j] = array[j-1]
-				j = j - 1
-			}
-
-			array[j] = last
-		}
+		array[j] = last
 	}
 
 	helper.PrintArray(array)
+
+	return array
 }
 
-func MixInsertionSort(array []interface{}, isDescending bool) {
+func MixInsertionSort(array []interface{}) {
 	floatArray, stringArray := helper.ClassifyArrayBasedOnType(array)
-	if !isDescending {
-		InsertionSort(floatArray, isDescending)
-		InsertionSort(stringArray, isDescending)
-	} else {
-		InsertionSort(stringArray, isDescending)
-		InsertionSort(floatArray, isDescending)
-	}
+	InsertionSort(floatArray)
+	InsertionSort(stringArray)
 }
